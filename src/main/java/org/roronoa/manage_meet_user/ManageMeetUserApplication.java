34848df3ba565f7bfd2d2254521aca976ac0d62e
Oussama Souayri <souayri.oussama@gmail.com>
2023-03-24 12:00:08 +0000

    init commit

---
+++ src/main/java/org/roronoa/manage_meet_user/ManageMeetUserApplication.java
@@ -0,0 +1,13 @@
+package org.roronoa.manage_meet_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageMeetUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageMeetUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/roronoa/manage_meet_user/ManageMeetUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.roronoa.manage_meet_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageMeetUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
